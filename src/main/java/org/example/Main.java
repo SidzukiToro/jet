468d9b13f41a55e60f7b44ac338fb351510fdad9
Toro <98184325+SidzukiToro@users.noreply.github.com>
2023-10-21 12:12:34 +0300

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String envVariableName = "A";
+
+        String envVariableValue = System.getenv(envVariableName);
+
+        if (envVariableValue != null) {
+            System.out.println("Значение переменной " + envVariableName + ": " + envVariableValue);
+        } else {
+            System.out.println("Переменная " + envVariableName + " не определена в окружении.");
+        }
+    }
+}
