1dbcb561b4013623475d3687f43daa5236a119fe
Toro <98184325+SidzukiToro@users.noreply.github.com>
2023-10-21 12:19:32 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        String envVariableName = "A";
+        String envVariableName = "OS";
 
         String envVariableValue = System.getenv(envVariableName);
 
